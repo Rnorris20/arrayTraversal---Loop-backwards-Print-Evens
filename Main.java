ef502043462cc1cffe1f65ac83e141d2f921d716
RNorris6 <ryan.norris@patelhs.org>
2020-11-05 23:39:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+
+    int[] numbers = {3,6,9,12,15,18,21,24,27,30};
+
+    for(int i=9; i>=0; i--){
+      if(numbers[i] % 2==0){
+
+        System.out.println(numbers[i]);
+      }
+    }
+  }
+}
